dffeff8c5181647fb42e9c5ceb00d9ba0930ebcb
Michée Kolony <micheekolony71@gmail.com>
2024-06-23 14:19:19 +0100

    third commit

--- server.js
+++ server.js
@@ -2,6 +2,6 @@
 const http = require('http');
 const app = require('./app');
 const port = 3000;
+app.set('port', process.env.PORT || port);
 const server = http.createServer(app);
-app.set('port', process.env.PORT || port)
 server.listen(process.env.PORT || port);
